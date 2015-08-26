7073e96ffd3616d75887550aa074a27098a61d71
EricSSartorius <EricSSartorius@gmail.com>
2015-08-26 19:27:45 +0900

    checkpoint 4 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
-angular.module('blocJams', ['ui.router'])
- .config(function($stateProvider, $locationProvider) {
+var blocJams = angular.module('blocJams', ['ui.router']);
+
+	blocJams.config(function($stateProvider, $locationProvider) {
 
  	    $locationProvider.html5Mode({
         enabled: true,
@@ -10,20 +11,33 @@ angular.module('blocJams', ['ui.router'])
 			.state('landing', {
 		    	url: '/landing',
 		    	controller: 'Landing.controller',
-		    	templateUrl: '/templates/landing.html'
+		    	templateUrl: '/templates/landing.html',
 			})
 			.state('album', {
 		        url: '/album',
 		        controller: 'Album.controller',
-		        templateUrl: '/templates/album.html'
+		        templateUrl: '/templates/album.html',
       		})
       		.state('collection', {
           		url: '/collection',
           		controller: 'Collection.controller',
-          		templateUrl: '/templates/collection.html'
+          		templateUrl: '/templates/collection.html',
       		});
+	});
+
+ blocJams.controller('Landing.controller', function ($scope) {
+ 	$scope.landingTitle = 'Turn the music up!';
  });
 
+ blocJams.controller('Album.controller', function ($scope) {
+ 	$scope.picasso = albumPicasso;
+ });
 
-
+ blocJams.controller('Collection.controller', function ($scope) {
+ 	$scope.picasso = albumPicasso;
+ 	$scope.number = 12;
+	$scope.getNumber = function(num) {
+    	return new Array(num);   
+	}
+ });
  
