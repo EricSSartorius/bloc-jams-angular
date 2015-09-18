7f9935a5952c0643dd5c1720aa8a88d4fb1606fb
EricSSartorius <EricSSartorius@gmail.com>
2015-09-18 22:12:34 +0900

    slider.html added, jump function created

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -256,74 +256,80 @@ angular.module('blocJams', ['ui.router'])
  //    if (currentSoundFile) {
  //        currentSoundFile.bind('timeupdate', function(event) {
  //            var seekBarFillRatio = this.getTime() / this.getDuration();
- //            var $seekBar = $('.seek-control .seek-bar');
- //            updateSeekPercentage($seekBar, seekBarFillRatio);
+ //            var element = $('.seek-control .seek-bar');
+ //            updateSeekPercentage(element, seekBarFillRatio);
  //            setCurrentTimeInPlayerBar(this.getTime());
  //            resetSong();
  //        });
  //    }
  // };
- // var updateSeekPercentage = function($seekBar, seekBarFillRatio) {
+ // var updateSeekPercentage = function(element, seekBarFillRatio) {
  //    var offsetXPercent = seekBarFillRatio * 100;
  //    offsetXPercent = Math.max(0, offsetXPercent);
  //    offsetXPercent = Math.min(100, offsetXPercent);
  //    var percentageString = offsetXPercent + '%';
- //    $seekBar.find('.fill').width(percentageString);
- //    $seekBar.find('.thumb').css({left: percentageString});
+ //    element.find('.fill').width(percentageString);
+ //    element.find('.thumb').css({left: percentageString});
  // };
 
      return {
-         templateUrl: 'templates/album.html',
+         templateUrl: 'templates/slider.html',
          restrict: 'E',
          replace: true,
-         scope: { },
-         controller: function() {
+         scope: { 
+            // value: ''
+         },
+         controller: function($scope) {
+
 
          },
          link: function(scope, element, attributes) {
-             // scope.value = 0;
-             // scope.doSomething = function() {
-             //     return { width: performAction() };
-             // };
-             // var $someElement = $(element);
-            element.bind('click', function () {
-                if (currentlyPlayingSongNumber === null) {
-                    return;
-                }
-                var offsetX = event.pageX - $(this).offset().left;
-                var barWidth = $(this).width();
+             scope.jump = function (event) {
+                // if (currentlyPlayingSongNumber === null) {
+                //     return;
+                // }
+                var offsetX = event.pageX - (element[0].getBoundingClientRect().left + document.body.scrollLeft);
+                var getDocumentWidth = function () {
+                    return Math.max(
+                        document.body.scrollWidth,
+                        document.documentElement.scrollWidth,
+                        document.body.offsetWidth,
+                        document.documentElement.offsetWidth,
+                        document.body.clientWidth,
+                        document.documentElement.clientWidth
+                    );
+                };
+                var barWidth = getDocumentWidth();
                 var seekBarFillRatio = offsetX / barWidth;
-                if ($(this).parent().attr('class') == 'seek-control') {
-                    seek(seekBarFillRatio * currentSoundFile.getDuration());
-                    }
-                else{
-                    setVolume(seekBarFillRatio * 100);
-                }
-                updateSeekPercentage($(this), seekBarFillRatio);
-            });
-            element.bind('mousedown', function () {
-                var $seekBar = $(this).parent();
-                $(document).bind('mousemove.thumb', function(event){
-                var offsetX = event.pageX - $seekBar.offset().left;
-                var barWidth = $seekBar.width();
-                var seekBarFillRatio = offsetX / barWidth;
-                if (currentlyPlayingSongNumber === null) {
-                    return;
-                }
-                if ($seekBar.parent().attr('class') == 'seek-control') {
-                    seek(seekBarFillRatio * currentSoundFile.getDuration()); 
-                }
-                else{
-                   setVolume(seekBarFillRatio);
-                }
-                updateSeekPercentage($seekBar, seekBarFillRatio);
-                });
-                $(document).bind('mouseup.thumb', function() {
-                $(document).unbind('mousemove.thumb');
-                $(document).unbind('mouseup.thumb');
-                });
-            });
-            var $seekBars = $('.player-bar .seek-bar');
+                // if (element.parent().attr('class') == 'seek-control') {
+                //     seek(seekBarFillRatio * currentSoundFile.getDuration());
+                //     }
+                // else{
+                //     setVolume(seekBarFillRatio * 100);
+                // }
+                // updateSeekPercentage(element, seekBarFillRatio);
+            };
+            // element.bind('mousedown', function () {
+            //     $(document).bind('mousemove.thumb', function(event){
+            //     var offsetX = event.pageX - element.offset().left;
+            //     var barWidth = element.width();
+            //     var seekBarFillRatio = offsetX / barWidth;
+            //     if (currentlyPlayingSongNumber === null) {
+            //         return;
+            //     }
+            //     if (element.parent().attr('class') == 'seek-control') {
+            //         seek(seekBarFillRatio * currentSoundFile.getDuration()); 
+            //     }
+            //     else{
+            //        setVolume(seekBarFillRatio);
+            //     }
+            //     updateSeekPercentage(element, seekBarFillRatio);
+            //     });
+            //     $(document).bind('mouseup.thumb', function() {
+            //     $(document).unbind('mousemove.thumb');
+            //     $(document).unbind('mouseup.thumb');
+            //     });
+            // });
          }
      };
  })
