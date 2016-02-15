be210484abf896366abd732ff47c3245cf64e0ff
EricSSartorius <EricSSartorius@gmail.com>
2016-02-14 19:20:39 -0500

    concat and minified css files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
 
-    grunt.registerTask( 'default', [ 'clean', 'copy', 'hapi', 'watch'] );
+    grunt.registerTask( 'default', [ 'clean', 'uglify', 'concat_css', 'cssmin', 'copy', 'hapi', 'watch'] );
 
-    grunt.registerTask( 'build', [ 'clean', 'copy' ] );
+    grunt.registerTask( 'build', [ 'clean', 'uglify', 'concat_css', 'cssmin', 'copy' ] );
 
     grunt.registerTask( 'run', [ 'hapi', 'watch' ]);
 
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
             hapi: {
                 files: [
                     './app/assets/**/*.{png,jpg,jpeg,mp3}',
-                    './app/scripts/min/**/*.js',
+                    './app/scripts/**/*.js',
                     './app/styles/**/*.css',
                     './app/pages/**/*.html',
                     './app/templates/**/*.html',
@@ -20,11 +20,45 @@ module.exports = function(grunt) {
                 ],
                 tasks: [
                     'clean',
+                    'uglify',
+                    'concat_css',
+                    'cssmin',
                     'copy'
                 ],
                 options: {
                     spawn: false
                 }
+            }
+        },
+
+        concat_css: {
+            options: {
+              // Task-specific options go here. 
+            },
+            all: {
+              src: ["./app/**/*.css"],
+              dest: "./app/styles/styles.css"
+            },
+        },
+
+        cssmin: {
+          target: {
+            files: [{
+              expand: true,
+              cwd: 'app/styles',
+              src: ['styles.css', '!*.min.css'],
+              dest: 'dist/styles/min',
+              ext: '.min.css'
+            }]
+          }
+        },
+
+        uglify: {
+            my_target: {
+              files: [{
+                './app/scripts/min/fixtures.min.js': ['./app/scripts/fixtures.js']},
+                {'./app/scripts/min/app.min.js': ['./app/scripts/app.js']
+              }]
             }
         },
 
@@ -40,16 +74,11 @@ module.exports = function(grunt) {
                     src: [ './**/*.html' ],
                     dest: './dist',
                     cwd: './app/pages'
-                }, {
-                    expand: true,
-                    src: [ './**/*.css' ],
-                    dest: './dist/styles',
-                    cwd: './app/styles'
-                }, {
+                },  {
                     expand: true,
                     src: [ './**/*.js' ],
                     dest: './dist/scripts',
-                    cwd: './app/scripts/min'
+                    cwd: './app/scripts'
                 }, {
                     expand: true,
                     src: [ './**/*.html' ],
@@ -77,5 +106,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-hapi');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-concat-css');
 
 };
